63f4ca3f80a8b3750873dc41ac543f2f9f65f6f8
Gustavo La Cruz <gscruz627@protonmail.com>
2023-07-05 10:47:50 -0500

    Small RWD issues
    Deleted extra words, increased navbar size on mobile devices, added padding to main container

--- client/views/HomeView.jsx
+++ client/views/HomeView.jsx
@@ -9,7 +9,7 @@ const HomeView = () => {
     <>
       <Navbar />
       <div className="home-container">
-        <h1>Create your own fictional order</h1>
+        <h1 style={{marginTop:"2rem"}}>Create your own fictional order</h1>
         <Link to="/makeorder"><button>BEGIN</button></Link>
         <img src="/cap1.png" />
         <img src="/cap2.png" />
@@ -22,15 +22,24 @@ const HomeView = () => {
         </p>
         <h3>Technical Description</h3>
         <p>
-          This web app was created using Express JS and MongoDB in the Backend,
-          as well as security and tools packages such as jsonwebtoken, mongoose,
-          helmet, etc. In the Front End this app uses React JS, compiled via
-          Vite, and using the Fetch API.
+          This web app was created using:<br/>
+          <ul style={{marginTop: "1rem"}}>
+            <li>- Node JS</li>
+            <li>- Express JS</li>
+            <li>- MongoDB</li>
+            <li>- JSONWebToken</li>
+            <li>- Other packages</li>
+          </ul>
+          <p style={{marginTop: "1rem"}}>And, </p>
+          <ul style={{marginTop: "1rem"}}>
+            <li>- Vite</li>
+            <li>- React JS</li>
+            <li>- Fetch API</li>
+          </ul> 
         </p>
         <h3>Contact</h3>
         <p>Contact at <a href="https://github.com/gscruz627">GitHub</a>.</p>
       </div>
-      <Link to="/makeorder">Build a new Order</Link>
     </>
   );
 };
